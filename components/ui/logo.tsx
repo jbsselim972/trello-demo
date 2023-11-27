f6b5bb8f737376b37f98c3b0a3fcd254441ce338
jbsselim <jbsselim@gmail.com>
2023-11-27 11:02:48 -0400

    corrected path

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -4,17 +4,19 @@ import localFont from "next/font/local";
 
 import { cn } from "@/lib/utils";
 
-const headingFont = localFont({ src: "../public/fonts/heading.woff2" });
+const headingFont = localFont({ src: "../../public/fonts/font.woff2" });
 
 const Logo = () => {
   return (
     <Link href="/">
       <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
         <Image src="/logo.svg" alt="Logo" height={30} width={30} />
+        <p
+          className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}
+        >
+          Taskify
+        </p>
       </div>
-      <p className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}>
-        Taskify
-      </p>
     </Link>
   );
 };
