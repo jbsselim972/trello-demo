07c13d65811f9d1406b23792cc9572ad3539e0a1
jbsselim <jbsselim@gmail.com>
2023-11-27 14:54:32 -0400

    changes

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -10,7 +10,7 @@ const Logo = () => {
   return (
     <Link href="/">
       <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
-        <Image src="/logo.svg" alt="Logo" height={30} width={30} />
+        <Image sizes="" src="/logo.svg" alt="Logo" height={30} width={30} />
         <p className={cn("text-lg text-neutral-700", headingFont.className)}>
           Taskify
         </p>
