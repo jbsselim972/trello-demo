66606217a2e6d01e91ef65b76eb83ace7f5850a4
jbsselim <jbsselim@gmail.com>
2023-11-29 12:33:40 -0400

    added BoardList component

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,17 @@
-const OrganizationIdPage = () => {
-  return <div>OrganizationPage</div>;
+import BoardList from "./_components/board-list";
+import { Info } from "./_components/info";
+import { Separator } from "@/components/ui/separator";
+
+const OrganizationIdPage = async () => {
+  return (
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
