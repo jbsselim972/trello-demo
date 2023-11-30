2f6a10d7f98a57dfc43febb303f749ec5fb3738b
jbsselim <jbsselim@gmail.com>
2023-11-30 10:31:43 -0400

    fix

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -32,7 +32,7 @@ export const BoardList = async () => {
         {boards.map((board) => (
           <Link
             key={board.id}
-            href={`board/${board.id}`}
+            href={`/board/${board.id}`}
             className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm h-full w-full p-2 overflow-hidden"
             style={{ backgroundImage: `url(${board.imageThumbUrl})` }}
           >
