cac59c73cc42989d837d5804c7df35443feb7fdf
jbsselim <jbsselim@gmail.com>
2023-11-30 16:49:54 -0400

    added form to create list

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+import { useState, useRef, ElementRef } from "react";
+import { PlusIcon, XIcon } from "lucide-react";
+import { useEventListener, useOnClickOutside } from "usehooks-ts";
+import { toast } from "sonner";
+
+import { ListWrapper } from "./list-wrapper";
+import { FormInput } from "@/components/form/form-input";
+import { FormSubmit } from "@/components/form/form-submit";
+import { Button } from "@/components/ui/button";
+
+import { useAction } from "@/hooks/use-action";
+import { createList } from "@/actions/create-list";
+
+export const ListForm = () => {
+  const params = useParams();
+  const router = useRouter();
+
+  const [isEditing, setIsEditing] = useState(false);
+  const formRef = useRef<ElementRef<"form">>(null);
+  const inputRef = useRef<ElementRef<"input">>(null);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const { execute, fieldErrors } = useAction(createList, {
+    onSuccess: (data) => {
+      toast.success(`List "${data.title} created"`);
+      disableEditing();
+      router.refresh();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      disableEditing();
+    }
+  };
+
+  useEventListener("keydown", onKeyDown);
+  useOnClickOutside(formRef, disableEditing);
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    const boardId = formData.get("boardId") as string;
+    execute({ title, boardId });
+  };
+
+  if (isEditing) {
+    return (
+      <ListWrapper>
+        <form
+          action={onSubmit}
+          ref={formRef}
+          className="w-full p-3 rounded-md bg-white space-y-4 shadow-md"
+        >
+          <FormInput
+            errors={fieldErrors}
+            ref={inputRef}
+            id="title"
+            className="text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition"
+            placeholder="Enter list title..."
+          />
+          <input hidden defaultValue={params.boardId} name="boardId" />
+          <div className="flex items-center gap-x-1">
+            <FormSubmit>Add list</FormSubmit>
+            <Button onClick={disableEditing} size="sm" variant="ghost">
+              <XIcon className="h-5 w-5" />
+            </Button>
+          </div>
+        </form>
+      </ListWrapper>
+    );
+  }
+  return (
+    <ListWrapper>
+      <button
+        onClick={enableEditing}
+        className="w-full rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex items-center font-medium text-sm"
+      >
+        <PlusIcon className="h-4 w-4 mr-2" />
+        Add a list
+      </button>
+    </ListWrapper>
+  );
+};
