df3e61ef3f62f8ee848ef89a7658fce9ae817434
jbsselim <jbsselim@gmail.com>
2023-11-30 16:51:37 -0400

    added card form to create a card in list

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
@@ -0,0 +1,53 @@
+import { forwardRef } from "react";
+import { PlusIcon, XIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { FormTextarea } from "@/components/form/form-textarea";
+import { FormSubmit } from "@/components/form/form-submit";
+
+interface CardFormProps {
+  listId: string;
+  enableEditing: () => void;
+  disableEditing: () => void;
+  isEditing: boolean;
+}
+
+export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
+  ({ listId, isEditing, enableEditing, disableEditing }, ref) => {
+    if (isEditing) {
+      return (
+        <form className="m-1 py-0.5 px-1 space-y-4">
+          <FormTextarea
+            id="title"
+            onKeyDown={() => {}}
+            ref={ref}
+            placeholder="Enter a title for this card..."
+          />
+          <input hidden id="listId" name="listId" value={listId} />
+          <div className="flex items-center gap-x-1">
+            <FormSubmit>Add card</FormSubmit>
+            <Button onClick={disableEditing} size="sm" variant="ghost">
+              <XIcon className="h-5 w-5" />
+            </Button>
+          </div>
+        </form>
+      );
+    }
+
+    return (
+      <div className="pt-2 px-2">
+        <Button
+          onClick={enableEditing}
+          className="h-auto px-2 py-1.5 w-full justify-start text-muted-foreground text-sm"
+          size="sm"
+          variant="ghost"
+        >
+          <PlusIcon className="h-4 w-4 mr-2" />
+          Add a card
+        </Button>
+      </div>
+    );
+  }
+);
+
+CardForm.displayName = "CardForm";
