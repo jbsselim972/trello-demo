8380710fdd354f0ce0acd853b8e5b03824aa77bf
jbsselim <jbsselim@gmail.com>
2023-11-30 10:31:17 -0400

    created layout w/ BoardNavbar w/ Input to update board title and Popover to delete board

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-options.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { MoreHorizontal, MoreHorizontalIcon, X, XIcon } from "lucide-react";
+import { useAction } from "@/hooks/use-action";
+import { deleteBoard } from "@/actions/delete-board";
+import { toast } from "sonner";
+
+interface BoardOptionsProps {
+  id: string;
+}
+
+export const BoardOptions = ({ id }: BoardOptionsProps) => {
+  const { execute, isLoading } = useAction(deleteBoard, {
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const onDelete = () => {
+    execute({ id });
+  };
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button className="h-auto w-auto p-2" variant="transparent">
+          <MoreHorizontalIcon className="h-4 w-4" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="px-0 pt-3 pb-3" side="bottom" align="start">
+        <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+          Board actions
+        </div>
+        <PopoverClose asChild>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
+            variant="ghost"
+          >
+            <XIcon className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+        <Button
+          disabled={isLoading}
+          variant="ghost"
+          onClick={onDelete}
+          className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+        >
+          Delete this board
+        </Button>
+      </PopoverContent>
+    </Popover>
+  );
+};
