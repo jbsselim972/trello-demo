e5882748e5a57dc57497188c258e359e68647420
jbsselim <jbsselim@gmail.com>
2023-12-01 14:33:16 -0400

    added modal

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,15 +1,20 @@
 import { Card } from "@prisma/client";
 import { Draggable } from "@hello-pangea/dnd";
+
+import { useCardModal } from "@/hooks/use-card-modal";
 interface CardItemProps {
   index: number;
   data: Card;
 }
 
 export const CardItem = ({ index, data }: CardItemProps) => {
+  const cardModal = useCardModal();
+
   return (
     <Draggable draggableId={data.id} index={index}>
       {(provided) => (
         <div
+          onClick={() => cardModal.onOpen(data.id)}
           {...provided.dragHandleProps}
           {...provided.draggableProps}
           ref={provided.innerRef}
