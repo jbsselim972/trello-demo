8c5cd491dbefe550d413e0bc77c395dd0b4b4056
jbsselim <jbsselim@gmail.com>
2023-12-01 16:12:00 -0400

    added helper functions to keep track of board number in create board and delete board actions

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -9,10 +9,23 @@ import { createSafeAction } from "@/lib/create-safe-action";
 import { createAuditLog } from "@/lib/create-audit-log";
 import { CreateBoard } from "./schema";
 import { InputType, ReturnType } from "./types";
+import { incrementAvailableCount, hasAvailableCount } from "@/lib/org-limit";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
-  if (!userId || !orgId) return { error: "Unauthorized" };
+
+  if (!userId || !orgId) {
+    return { error: "Unauthorized" };
+  }
+
+  const canCreate = await hasAvailableCount();
+
+  if (!canCreate) {
+    return {
+      error:
+        "You have reached your limit of free boards. Please upgrade to create more.",
+    };
+  }
 
   const { title, image } = data;
   const [imageId, imageThumbUrl, imageFullUrl, imageLinkHtml, imageUserName] =
@@ -43,6 +56,8 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         imageUserName,
       },
     });
+
+    await incrementAvailableCount();
 
     await createAuditLog({
       entityId: board.id,
